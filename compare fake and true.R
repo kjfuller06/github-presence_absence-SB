0834542b77ed65d33a00e7636add1a116611df80
drvalle@ufl.edu <drvalle@ufl.edu>
2017-10-25 15:37:09 -0400

    Commit 9
    I have eliminated the k-means initial starting values. While this worked
    for ncomm=10, it did not work for ncomm=5. Therefore, I have reverted
    back to randomly initializing phi, theta, and z. This seems to work well
    for the simulated data

--- compare fake and true.R
+++ compare fake and true.R
@@ -1,26 +1,34 @@
-plot(vec.logl,type='l')
+plot(llk[9000:ngibbs],type='l')
 
 #order=black,red,green,blue,light blue
-ind=1:ncomm#c(1,2,5,6,10) #1:5#
-theta=matrix(res$theta[nrow(res$theta),],nloc,ncomm)
-theta1=param$theta[,ind]
-phi=matrix(res$phi[nrow(res$phi),],ncomm,nspp)
-phi1=param$phi[ind,]
+ind=1:ncomm
+theta1=theta[,ind]
 
 boxplot(theta1)
 plot(NA,NA,ylim=c(0,1),xlim=c(0,nloc))
 for (i in 1:ncomm) lines(theta1[,i],col=i)
 
-rango=range(c(phi.true,phi1))
-plot(phi.true,phi1,xlim=rango,ylim=rango)
-lines(rango,rango)
-
-rango=range(c(theta.true,theta1))
-plot(theta.true,theta1[,ind],xlim=rango,ylim=rango)
-lines(rango,rango)
-#----------------------------------------
 par(mfrow=c(2,1),mar=rep(1,4))
 plot(NA,NA,ylim=c(0,1),xlim=c(0,nloc))
 for (i in 1:5) lines(theta1[,i],col=i)
 plot(NA,NA,ylim=c(0,1),xlim=c(0,nloc))
 for (i in 6:10) lines(theta1[,i],col=i)
+
+#---------------------------------------------
+#COMPARE LIKELIHOOD 
+#it is complicated to compare log-prior because ncol(theta.true)=5 and not 10 and therefore sum(dbeta(vmat)) will be different
+
+#true likelihood
+setwd('U:\\presence absence model\\simulations') 
+ncommun=5
+nome=paste(c('fake data ','theta ','phi '),ncommun,'.csv',sep='')
+theta.true=data.matrix(read.csv(nome[2],as.is=T))
+phi.true=data.matrix(read.csv(nome[3],as.is=T))
+
+prob=theta.true%*%phi.true
+prob[prob>hi]=hi; prob[prob<lo]=lo
+prob1=prob[loc.id,]
+true.logl=sum(dat1*log(prob1)+(1-dat1)*log(1-prob1))
+plot(llk,type='l',ylim=range(c(llk,true.logl)))
+abline(h=true.logl,col='red')
+
--- gibbs functions.R
+++ gibbs functions.R
@@ -1,5 +1,15 @@
-#BASE FUNCTIONS 
-
+#----------------------------------------------------------------
+get.theta=function(nlk,gamma,ncomm,nloc){
+  vmat=matrix(NA,nloc,ncomm)
+  for (i in 1:(ncomm-1)){
+    if (i==(ncomm-1)) cumsoma=nlk[,ncomm]
+    if (i< (ncomm-1)) cumsoma=rowSums(nlk[,(i+1):ncomm])
+    vmat[,i]=rbeta(nloc,nlk[,i]+1,cumsoma+gamma)
+  }
+  vmat[,ncomm]=1
+  convertVtoTheta(vmat,rep(1,nloc))
+}
+#------------------------------------------------------
 aggregate.data=function(dat){
   dat1=dat[order(dat$loc.id),]
   nloc=max(dat1$loc.id)
@@ -12,141 +22,11 @@ aggregate.data=function(dat){
     cond=dat1$loc.id==i
     dat.tmp=dat1[cond,-ind]
     n[i]=nrow(dat.tmp)
-    if (n[i]>1) dat.tmp=colSums(dat.tmp)
+    if (n[i]>1)  dat.tmp=colSums(dat.tmp)
     if (n[i]==1) dat.tmp=as.numeric(dat.tmp)
     res[i,]=dat.tmp
   }
   colnames(res)=colnames(dat1[,-ind])
   loc.id=unique(dat1$loc.id)
   list(dat=res,loc.id=loc.id,n=n)
-}
-#----------------------------------------------
-print.adapt = function(accept1,jump1,accept.output){
-
-  for (k in 1:length(accept1)){
-    z=accept1[[k]]/accept.output
-    print(names(accept1)[k])
-    print(mean(z)); print(mean(jump1[[k]]))
-  }
-  
-  for (k in 1:length(jump1)){
-    cond=(accept1[[k]]/accept.output)>0.4 & jump1[[k]]<1
-    jump1[[k]][cond] = jump1[[k]][cond]*2       
-    cond=(accept1[[k]]/accept.output)<0.2 & jump1[[k]]>0.005
-    jump1[[k]][cond] = jump1[[k]][cond]*0.5
-    accept1[[k]][]=0
-  }
-  
-  return(list(jump1=jump1,accept1=accept1))
-}
-#----------------------------
-fix.MH=function(lo,hi,old1,new1,jump){
-  jold=pnorm(hi,mean=old1,sd=jump)-pnorm(lo,mean=old1,sd=jump)
-  jnew=pnorm(hi,mean=new1,sd=jump)-pnorm(lo,mean=new1,sd=jump)
-  log(jold)-log(jnew) #add this to pnew
-}
-#----------------------------------------------------------------------------------------------
-tnorm <- function(n,lo,hi,mu,sig){   #generates truncated normal variates based on cumulative normal distribution
-  #normal truncated lo and hi
-  
-  if(length(lo) == 1 & length(mu) > 1)lo <- rep(lo,length(mu))
-  if(length(hi) == 1 & length(mu) > 1)hi <- rep(hi,length(mu))
-  
-  q1 <- pnorm(lo,mu,sig) #cumulative distribution
-  q2 <- pnorm(hi,mu,sig) #cumulative distribution
-  
-  z <- runif(n,q1,q2)
-  z <- qnorm(z,mu,sig)
-  z[z == -Inf]  <- lo[z == -Inf]
-  z[z == Inf]   <- hi[z == Inf]
-  z
-}
-#----------------------------------------------------------------------------------------------
-acceptMH <- function(p0,p1,x0,x1,BLOCK){   #accept for M, M-H
-  # if BLOCK, then accept as a block,
-  # otherwise, accept individually
-  
-  nz           <- length(x0)  #no. to accept
-  if(BLOCK) nz <- 1
-  
-  a    <- exp(p1 - p0)       #acceptance PR
-  z    <- runif(nz,0,1)
-  keep <- which(z < a)
-  
-  if(BLOCK & length(keep) > 0) x0 <- x1
-  if(!BLOCK)                   x0[keep] <- x1[keep]           
-  accept <- length(keep)        
-  
-  list(x = x0, accept = accept)
-}
-#----------------------------
-fix.probs=function(probs){
-  cond=probs<0.00001
-  probs[cond]=0.00001
-  cond=probs>0.99999
-  probs[cond]=0.99999
-  probs
-}
-#----------------------------
-get.logl=function(theta,phi,y,nmat){
-  prob=fix.probs(theta%*%phi)
-  y*log(prob)+(nmat-y)*log(1-prob)
-}
-
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-#FULL CONDITIONAL DISTRIBUTION FUNCTIONS 
-#----------------------------
-update.phi=function(param,jump,ncomm,nspp,y,nmat,a.phi,b.phi){
-  phi.orig=phi.old=param$phi
-  
-  proposed=matrix(tnorm(nspp*ncomm,lo=0,hi=1,mu=phi.old,sig=jump),ncomm,nspp)
-  prior.old=dbeta(phi.old,a.phi,b.phi,log=T)
-  prior.new=dbeta(proposed,a.phi,b.phi,log=T)
-  adj=fix.MH(lo=0,hi=1,old1=phi.old,new1=proposed,jump=jump)
-  
-  for (i in 1:ncomm){
-    phi.new=phi.old
-    phi.new[i,]=proposed[i,]
-
-    prob.old=get.logl(theta=param$theta,phi=phi.old,y=y,nmat=nmat)
-    prob.new=get.logl(theta=param$theta,phi=phi.new,y=y,nmat=nmat)
-    pold=colSums(prob.old)+prior.old[i,]
-    pnew=colSums(prob.new)+prior.new[i,]
-    k=acceptMH(p0=pold,p1=pnew+adj[i,],x0=phi.old[i,],x1=phi.new[i,],BLOCK=F)
-    phi.old[i,]=k$x
-  }
-  list(phi=phi.old,accept=phi.orig!=phi.old)
-}
-#-----------------------------
-update.theta=function(param,jump,nloc,ncomm,y,nmat,gamma){
-  v.orig=v.old=param$vmat
-  tmp=tnorm(nloc*(ncomm-1),lo=0,hi=1,mu=v.old[,-ncomm],sig=jump[,-ncomm])
-  novos=cbind(matrix(tmp,nloc,ncomm-1),1)
-  ajuste=matrix(fix.MH(lo=0,hi=1,old1=v.old,new1=novos,jump=jump),nloc,ncomm)
-  
-  prior.old=matrix(dbeta(v.old,1,gamma,log=T),nloc,ncomm)
-  prior.new=matrix(dbeta(novos,1,gamma,log=T),nloc,ncomm)
-  
-  for (j in 1:(ncomm-1)){ #last column has to be 1
-    v.new=v.old
-    v.new[,j]=novos[,j]
-    
-    theta.old=convertSBtoNormal(vmat=v.old,ncol=ncomm,nrow=nloc,prod=rep(1,nloc))
-    theta.new=convertSBtoNormal(vmat=v.new,ncol=ncomm,nrow=nloc,prod=rep(1,nloc))
-    
-    pold=get.logl(theta=theta.old,phi=param$phi,y=y,nmat=nmat)
-    pnew=get.logl(theta=theta.new,phi=param$phi,y=y,nmat=nmat)
-    p1.old=rowSums(pold)
-    p1.new=rowSums(pnew)
-    
-    k=acceptMH(p0=p1.old+prior.old[,j],
-               p1=p1.new+prior.new[,j]+ajuste[,j],
-               x0=v.old[,j],x1=v.new[,j],F)
-    v.old[,j]=k$x
-  }
-  theta=convertSBtoNormal(vmat=v.old,ncol=ncomm,nrow=nloc,prod=rep(1,nloc))
-  list(theta=theta,v=v.old,accept=v.old!=v.orig)
 }
